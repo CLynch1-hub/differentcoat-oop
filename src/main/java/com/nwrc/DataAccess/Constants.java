aa901656b4d8e6540b07d8b6b38e96ef687ab456
CLynch1-hub <65855281+CLynch1-hub@users.noreply.github.com>
2025-12-23 15:20:21 +0000

    Add files via upload

---
+++ src/main/java/com/nwrc/DataAccess/Constants.java
@@ -0,0 +1,12 @@
+package com.nwrc.DataAccess;
+
+public class Constants {
+
+    public static final int WINDOW = 25;  // set as 25 sq Feet
+    public static final int DOOR = 20;   // set as 20 Sq Feet
+
+    public static final int COVERAGE = 350; // Coverage per gallon in square feet
+
+    public static final double LITRES = 4.54;  // Rate of Litres to Gallons.. PaintArea / Coverage = Gallons
+
+}
